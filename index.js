f25b70c91646d7604aa556a21809ee55a883f697
Ting Chung <tchung682@gmail.com>
2020-05-15 18:01:14 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ dodger.style.left // "180px"
 dodger.style.bottom // "0px"
 dodger.style.bottom = '100px'
 dodger.style.bottom = '0px'
-document.addEventListener('keydown', function(e) {
-  console.log(e.which)
-})
+//document.addEventListener('keydown', function(e) {
+//  console.log(e.which)
+//})
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
@@ -29,5 +29,14 @@ function moveDodgerLeft() {
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
+    moveDodgerRight()
   }
 })
+function moveDodgerRight() {
+  var rightNumbers = dodger.style.right.replace('px', '')
+  var right = parseInt(rightNumbers, 10)
+
+  if (right > 0) {
+    dodger.style.right = `${right - 1}px`
+  }
+}
