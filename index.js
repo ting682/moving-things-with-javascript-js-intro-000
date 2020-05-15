3a761a9d46403db55678ae28e5bff35bd48db0f9
Ting Chung <tchung682@gmail.com>
2020-05-15 17:57:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     var leftNumbers = dodger.style.left.replace('px', '')
     var left = parseInt(leftNumbers, 10)
- 
+
     dodger.style.left = `${left - 1}px`
   }
 })
@@ -21,7 +21,7 @@ var dodger = document.getElementById('dodger')
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
   var left = parseInt(leftNumbers, 10)
- 
+
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
   }
