80c1d07f78a67907c8eaa5df95fabb4d3f34b49c
Ting Chung <tchung682@gmail.com>
2020-05-15 18:01:54 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ function moveDodgerLeft() {
 document.addEventListener('keydown', function(e) {
   if (e.which === 37) {
     moveDodgerLeft()
-    
+
   }
   if (e.which === 39) {
     moveDodgerRight()
